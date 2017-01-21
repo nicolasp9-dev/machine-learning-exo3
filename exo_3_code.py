3dc721835e3217b01c8a0d904f122c0119940618
Administrateur <development@peslerbe.com>
2017-01-21 14:33:05 +0100

    first upload

---
+++ exo_3_code.py
@@ -0,0 +1,86 @@
+#!/usr/bin/python
+
+
+# Seyed Saeid Masoumzadeh, Nicolas Peslerbe
+# Exercise 3 implementation
+
+
+# Needed libraries
+# Scipy
+# Arff :  easy_install liac-arff
+
+import sys, getopt
+import arff, numpy as np
+
+
+#
+# Load .ARFF file into a numpy table
+#
+# Input parameter : 
+#	dataset = (Mandatory) file name as String
+# Return value : numpy array
+#
+
+def load_file(datasetfile):
+	try :
+		dataset = arff.load(open(datasetfile, 'rb'))
+	except :
+		print 'Impossible to find or load :',datasetfile
+		sys.exit(4)
+	data = np.array(dataset['data'])
+	return data
+
+
+#
+# Get parameters entered into the terminal
+#
+# Input parameter :
+#	arv = (Mandatory) input arguments for the terminal 
+# Return value :
+#	dataset path as string
+#
+
+def options_load(argv) :
+	datasetfile = ''	
+	try:
+      		opts, args = getopt.getopt(argv,"hd:",["help","datasetfile="])
+   	except getopt.GetoptError:
+      		print 'Error'
+		sys.exit(2)
+
+	for opt, arg in opts:
+
+	      	if opt in ("-h", "--help"):
+	         	print get_help_txt()
+	        	sys.exit()
+
+	     	elif opt in ("-d", "--datasetfile"):
+	         	datasetfile = arg
+	if datasetfile == '' :
+		print '\nMandatory arguments are missing\nPlease enter : -h or --help for help\n'
+		sys.exit(3)
+	return datasetfile
+
+
+
+#
+# Function that return the complete text for help
+#
+
+def get_help_txt() :
+	return 	"""
+Help :
+-d or --datasetfile <path of dataset> : (Mandatory) Path of the dataset you want to work with
+		"""
+
+
+
+# Main function
+
+def main(argv):
+   	datasetfile = options_load(argv)
+	data  = load_file(datasetfile)
+	print data
+
+if __name__ == "__main__":
+   	main(sys.argv[1:])
---
+++ running.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# explanation of the running configuration
+python exo_3_code.py -d ISMIRgenre/ISMIRgenre.rp.arff
